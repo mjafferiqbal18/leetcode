6b2242bef2a0ac832c6daac82262fcaac1a8fbd4
mjafferiqbal18 <mjafferiqbal18@gmail.com>
2025-12-29 17:51:17 +0500

    first probelm

---
+++ 1_Arrays_and_Hashing/1_concatenation_of_an_array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        """
+        Problem: 1929. Concatenation of Array
+        https://leetcode.com/problems/concatenation-of-array/description/
+
+
+        Intuition:
+        - Copy over the original nums, iterate over it and append to it
+        
+        Time Complexity:
+        - O(n)
+
+        Space Complexity:
+        - O(2n) -> O(n)
+        """
+        res=nums.copy()
+        for n in nums:
+            res.append(n)
+        return res
